3a94d309a32ed408e98a0d4cd6be40ac3d97be15
jjanicechen <jjanicechen2013@gmail.com>
2016-03-06 18:34:10 +0800

    add gulpfile.js and package.json

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var watchify = require('watchify');
+var source = require('vinyl-source-stream');
+var gutil = require('gulp-util');
+var watch = require('gulp-watch');
+var livereload = require('gulp-livereload');
+var sass = require('gulp-sass');
+
+gulp.task('sass_main', function() {
+    gulp.src('./src/css/main.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('public'));
+});
+
+gulp.task('sass_tile', function() {
+    gulp.src('./src/css/tile.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('public'));
+});
+
+gulp.task('jsx', () => {
+		var bundler = browserify('./src/jsx/App.jsx', {
+				debug: true,
+				paths: ['./src/jsx'],
+				cache: {},
+				packageCache: {},
+				fullPaths: true
+		}).transform(babelify, {
+				presets: ["es2015", "react"],
+				ignore: /(bower_components)|(node_modules)/
+		});
+
+		function rebundle() {
+				gutil.log('jsx starting.');
+				bundler
+						.bundle()
+						.on('error', err => { gutil.log(err.message); })
+						.pipe(source('bundle.js'))
+						.pipe(gulp.dest('./public'))
+						.pipe(livereload())
+						.on('end', () => { gutil.log('jsx completed.'); });
+		}
+
+		bundler = watchify(bundler);
+		rebundle();
+		bundler.on('update', rebundle);
+});
+
+gulp.task('static', () => {
+		gulp.src(['./src/index.html', './src/*.*'])
+				.pipe(gulp.dest('./public'));
+});
+
+gulp.task('watch', () => {
+		watch(['./src/index.html', './src/*.*'], () => { gulp.start('static'); });
+});
+
+gulp.task('default', ['watch', 'jsx', 'static', 'sass_main', 'sass_tile']);
